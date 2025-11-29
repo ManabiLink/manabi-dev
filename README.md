4e307d1976f13d3a5fc490a86cf980030efb769e
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-11-29 20:29:07 +0900

    初期設定の完了

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # manabi-dev
-開発者専用画面
+manabi-dev is a development environment for the manabi learning management system (LMS). It provides tools and configurations to facilitate the development, testing, and deployment of manabi LMS.
+
+## Authors
+
+Lead: s-taku0502
+Team Members: @harurun-522 , @wakigen , @kakusei777 , @yamakan20050909214-alt , @tanaka308
+
+## LICENSE
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "まなびリンク管理者用ページ",
+  description: "まなびリンクの管理者用ページです。事前の認証が必要です。",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ja">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
